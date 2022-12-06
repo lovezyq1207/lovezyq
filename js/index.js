ba9de659a26f9969fff216e19347e916a5199869
lovezyq <2001212928@stu.pku.edu.cn>
2022-12-07 00:20:31 +0800

    111

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ $("#login-button").click(function (event) {
   var userName = document.getElementById("userName").value;
   var pwd = document.getElementById("pwd").value;
   //修改密码请改此处
-  if (userName == "月琪" && pwd == "1207") {
+  if (userName == "月琪公主" && pwd == "1207") {
     event.preventDefault();
     $("form").fadeOut(500);
     $(".wrapper").addClass("form-success");
